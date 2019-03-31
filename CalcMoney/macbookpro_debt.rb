824a47b42bd0789b713cc26866eb77cd1993086a
shuta13 <hollystarsun@gmail.com>
2019-03-31 22:06:54 +0900

    SSD1TB積んじゃった

--- CalcMoney/macbookpro_debt.rb
+++ CalcMoney/macbookpro_debt.rb
@@ -1,34 +1,32 @@
-subtotal = 320400
-tax = 24192
+subtotal = 360000
+tax = 27360
 sum = subtotal + tax
 
 p "小計：#{subtotal}"
 p "税：#{tax}"
 p "合計：#{sum}"
 
-discount = 22000
-sum = sum - discount
-p "学割後：#{sum}"
-
-money_down = 60000
+money_down = 60360
 sum = sum - money_down
 p "頭金後：#{sum}"
 
-pay_par_month = 11000
-extra = 1408
+pay_par_month = 15000
+extra = 12000
 
-p "24回分割：#{sum / 24}のため、#{pay_par_month}ずつ返済"
+p "21回分割：#{pay_par_month}ずつ返済"
 p "+#{extra}円余分のため最終月で調整"
-p '=> ' + ((sum + extra) == (pay_par_month * 24)).to_s
 
 month = 4
 year = 2019
 p "--- #{year}年度 ---"
 
+
+counter = 0
 while true do 
   sum = sum - pay_par_month
   p "#{month}月残額：#{sum}"
   month += 1
+  counter += 1
   if month > 12
     month = 1
     year += 1
@@ -40,3 +38,4 @@ while true do
   end
 end
 
+p "合計#{counter}+1"
